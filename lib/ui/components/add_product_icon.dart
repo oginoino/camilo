ba9fd6b2a242f8198032b012231b49bc70033be6
Gino <gino.laranjeiras@onikel.com>
2024-04-14 16:17:33 -0300

    Atualizar posição do ícone de adicionar produto em AddProductIcon.dart

--- lib/ui/components/add_product_icon.dart
+++ lib/ui/components/add_product_icon.dart
@@ -19,8 +19,67 @@ class AddProductIcon extends StatelessWidget {
           color: Theme.of(context).colorScheme.secondary,
           size: uiConstants.iconSizeLarge,
         ),
-        onPressed: () {},
+        onPressed: () {
+          _buildAddProductTooltip(context);
+        },
       ),
     );
   }
+}
+
+void _buildAddProductTooltip(BuildContext context) {
+  final RenderBox renderBox = context.findRenderObject() as RenderBox;
+
+  final position = RelativeRect.fromLTRB(
+    renderBox
+        .localToGlobal(Offset(
+          renderBox.size.width - 120,
+          renderBox.size.height - 120,
+        ))
+        .dx,
+    renderBox.localToGlobal(Offset.zero).dy,
+    renderBox.localToGlobal(Offset.zero).dx,
+    renderBox.localToGlobal(Offset.zero).dy,
+  );
+
+  showMenu(
+    constraints: const BoxConstraints(maxWidth: 120),
+    context: context,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(50),
+    ),
+    position: position,
+    items: [
+      PopupMenuItem(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            IconButton(
+                visualDensity: VisualDensity.compact,
+                padding: EdgeInsets.zero,
+                onPressed: () {},
+                icon: Icon(
+                  Icons.remove_rounded,
+                  size: uiConstants.iconSizeSmall,
+                )),
+            Text(
+              '1',
+              style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                  color: Theme.of(context).colorScheme.tertiary,
+                  fontWeight: FontWeight.bold),
+            ),
+            IconButton(
+                visualDensity: VisualDensity.compact,
+                padding: EdgeInsets.zero,
+                onPressed: () {},
+                icon: Icon(
+                  Icons.add_rounded,
+                  size: uiConstants.iconSizeSmall,
+                ))
+          ],
+        ),
+      ),
+    ],
+  );
 }
