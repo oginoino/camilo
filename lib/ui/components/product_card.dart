797b513c73901da225e8504476925c0182f15eb6
Gino <gino.laranjeiras@onikel.com>
2024-04-14 13:35:19 -0300

    Corrigir largura e altura do contêiner no ProductCard

--- lib/ui/components/product_card.dart
+++ lib/ui/components/product_card.dart
@@ -14,31 +14,57 @@ class ProductCard extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(uiConstants.paddingMedium),
       ),
-      child: Column(
+      child: Stack(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(uiConstants.paddingMedium),
-                topRight: Radius.circular(uiConstants.paddingMedium),
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(uiConstants.paddingMedium),
+                    topRight: Radius.circular(uiConstants.paddingMedium),
+                  ),
+                ),
+                width: contaninerWidth,
+                height: contaninerHeght,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(uiConstants.paddingMedium),
+                    topRight: Radius.circular(uiConstants.paddingMedium),
+                  ),
+                  child: Image.network(
+                    'https://via.placeholder.com/200',
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-            ),
-            width: contaninerWidth,
-            height: contaninerHeght,
-            child: ClipRRect(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(uiConstants.paddingMedium),
-                topRight: Radius.circular(uiConstants.paddingMedium),
-              ),
-              child: Image.network(
-                'https://via.placeholder.com/200',
-                fit: BoxFit.cover,
-              ),
-            ),
+              const Text('Produto 1'),
+              const Text('R\$ 10,00'),
+            ],
           ),
-          const Text('Produto 1'),
-          const Text('R\$ 10,00'),
+          const AddProductIcon(),
         ],
+      ),
+    );
+  }
+}
+
+class AddProductIcon extends StatelessWidget {
+  const AddProductIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 0,
+      right: 0,
+      child: IconButton(
+        icon: Icon(
+          Icons.add_circle_rounded,
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+        onPressed: () {},
       ),
     );
   }
