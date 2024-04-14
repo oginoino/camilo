a067020e9070e8d85b2e623b4a886786f6b3bac6
Gino <gino.laranjeiras@onikel.com>
2024-04-14 14:51:42 -0300

    Corrigir largura e altura do contêiner no ProductCard

--- lib/ui/components/product_card.dart
+++ lib/ui/components/product_card.dart
@@ -1,5 +1,3 @@
-import 'package:camilo/main.dart';
-
 import '../../common_libs.dart';
 
 class ProductCard extends StatelessWidget {
@@ -11,20 +9,26 @@ class ProductCard extends StatelessWidget {
   Widget build(BuildContext context) {
     double contaninerWidth = 160.0;
     double contaninerHeght = 160.0;
+    double cardElevation = 0.0;
 
     const String productUnitOfMeasurement = 'unidade(s)';
-    const String productQuantity = '1';
-    const String productName = 'Produto 1';
+    const String productUnitQuantity = '1';
+    const String productName =
+        'Sorvete Ben & Jerry\'s morango com pedaços de chocolate e biscoito';
     const String productPrice = '10,00';
-    const String productImageSrc = 'https://via.placeholder.com/200';
+    const String productImageSrc =
+        'https://acdn.mitiendanube.com/stores/001/165/503/products/coca-zero21-16e7cba0588363da7616192142363168-640-0.png';
 
     return Card(
+      color: Theme.of(context).colorScheme.background,
+      elevation: cardElevation,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(uiConstants.paddingMedium),
       ),
       child: Stack(
         children: [
           Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
@@ -47,33 +51,48 @@ class ProductCard extends StatelessWidget {
                   ),
                 ),
               ),
-              Padding(
-                padding:
-                    EdgeInsets.symmetric(horizontal: uiConstants.paddingSmall),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          productQuantity,
-                          style: Theme.of(context).textTheme.labelSmall,
+              SizedBox(
+                height: 100,
+                width: contaninerWidth,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: uiConstants.paddingSmall),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            productUnitQuantity,
+                            style: Theme.of(context).textTheme.labelSmall,
+                          ),
+                          SizedBox(width: uiConstants.paddingExtraSmall),
+                          Text(
+                            productUnitOfMeasurement,
+                            style: Theme.of(context).textTheme.labelSmall,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 40,
+                        child: Text(
+                          productName,
+                          style: Theme.of(context).textTheme.labelMedium,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          semanticsLabel: productName,
                         ),
-                        SizedBox(width: uiConstants.paddingExtraSmall),
-                        Text(
-                          productUnitOfMeasurement,
-                          style: Theme.of(context).textTheme.labelSmall,
-                        ),
-                      ],
-                    ),
-                    Text(
-                      productName,
-                      style: Theme.of(context).textTheme.labelLarge,
-                    ),
-                    Text('R\$ $productPrice',
-                        style: Theme.of(context).textTheme.bodySmall),
-                  ],
+                      ),
+                      Text(
+                        'R\$ $productPrice',
+                        style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                              color: Theme.of(context).colorScheme.tertiary,
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
