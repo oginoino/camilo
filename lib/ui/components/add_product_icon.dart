ed5e74f43f47835d7610ea5ecc249f074dc53ff8
Gino <gino.laranjeiras@onikel.com>
2024-04-15 11:45:10 -0300

    Corrigir estilo do ícone de adicionar produto na classe AddProductIcon

--- lib/ui/components/add_product_icon.dart
+++ lib/ui/components/add_product_icon.dart
@@ -79,57 +79,52 @@ class AddProductIcon extends StatelessWidget {
         PopupMenuItem(
           padding: EdgeInsets.zero,
           height: 0,
-          child: TapRegion(
-            onTapOutside: (tap) {
-              FocusScope.of(context).unfocus();
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  IconButton(
-                      visualDensity: VisualDensity.compact,
-                      padding: EdgeInsets.zero,
-                      tooltip: 'Remover',
-                      onPressed: () {
-                        updateProductSelectedQuantity(isIncrement: false);
-                      },
-                      icon: Icon(
-                        product.selectedQuantity == 1
-                            ? Icons.delete_rounded
-                            : Icons.remove_rounded,
-                        size: uiConstants.iconSizeSmall,
-                        color: Theme.of(context).colorScheme.tertiary,
-                      )),
-                  Consumer<ProductList>(builder: (context, products, child) {
-                    return Text(
-                        products
-                            .getProductById(product.id)
-                            .selectedQuantity
-                            .toString(),
-                        style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                              color: Theme.of(context).colorScheme.tertiary,
-                              fontWeight: FontWeight.bold,
-                            ));
-                  }),
-                  IconButton(
-                      visualDensity: VisualDensity.compact,
-                      padding: EdgeInsets.zero,
-                      tooltip: 'Adicionar',
-                      onPressed: () {
-                        updateProductSelectedQuantity(isIncrement: true);
-                      },
-                      icon: Icon(
-                        Icons.add_rounded,
-                        size: uiConstants.iconSizeSmall,
-                        color: Theme.of(context).colorScheme.tertiary,
-                      ))
-                ],
-              ),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
+                    visualDensity: VisualDensity.compact,
+                    padding: EdgeInsets.zero,
+                    tooltip: 'Remover',
+                    onPressed: () {
+                      updateProductSelectedQuantity(isIncrement: false);
+                    },
+                    icon: Icon(
+                      product.selectedQuantity == 1
+                          ? Icons.delete_rounded
+                          : Icons.remove_rounded,
+                      size: uiConstants.iconSizeSmall,
+                      color: Theme.of(context).colorScheme.tertiary,
+                    )),
+                Consumer<ProductList>(builder: (context, products, child) {
+                  return Text(
+                      products
+                          .getProductById(product.id)
+                          .selectedQuantity
+                          .toString(),
+                      style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                            color: Theme.of(context).colorScheme.tertiary,
+                            fontWeight: FontWeight.bold,
+                          ));
+                }),
+                IconButton(
+                    visualDensity: VisualDensity.compact,
+                    padding: EdgeInsets.zero,
+                    tooltip: 'Adicionar',
+                    onPressed: () {
+                      updateProductSelectedQuantity(isIncrement: true);
+                    },
+                    icon: Icon(
+                      Icons.add_rounded,
+                      size: uiConstants.iconSizeSmall,
+                      color: Theme.of(context).colorScheme.tertiary,
+                    ))
+              ],
             ),
           ),
         ),
