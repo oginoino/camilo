00283cd85440cbdd8ba107c4f2e68fd7682a8a1d
Gino <gino.laranjeiras@onikel.com>
2024-04-15 17:36:12 -0300

    Corrigir problemas relacionados ao carrinho de compras e à exibição de preços

--- lib/ui/pages/cart_page.dart
+++ lib/ui/pages/cart_page.dart
@@ -11,125 +11,150 @@ class CartPage extends StatelessWidget {
 
     return Consumer<ProductCart>(
       builder: (context, cart, child) {
-        return CustomScrollView(
-          slivers: [
-            SliverAppBar(
-                title: const Text('Seu carrinho'),
-                leading: const BackButton(
-                  style: ButtonStyle(),
-                ),
-                floating: true,
-                snap: true,
-                pinned: false,
-                forceElevated: false,
-                expandedHeight: expandedHeight,
-                collapsedHeight: collapsedHeight,
-                flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: false,
-                  collapseMode: CollapseMode.parallax,
-                  expandedTitleScale: expandedTitleScale,
-                  titlePadding: EdgeInsets.zero,
-                  title: cart.products.products.isEmpty
-                      ? const SizedBox()
-                      : Padding(
-                          padding: EdgeInsets.all(uiConstants.paddingSmall),
-                          child: Align(
-                            alignment: Alignment.bottomRight,
-                            child: ElevatedButton.icon(
-                              style: ElevatedButton.styleFrom(
-                                  visualDensity: VisualDensity.compact),
-                              onPressed: () {
-                                Provider.of<ProductCart>(context, listen: false)
-                                    .clearProducts();
+        return Scaffold(
+          body: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                  title: const Text('Seu carrinho'),
+                  leading: const BackButton(
+                    style: ButtonStyle(),
+                  ),
+                  floating: true,
+                  snap: true,
+                  pinned: false,
+                  forceElevated: false,
+                  expandedHeight: expandedHeight,
+                  collapsedHeight: collapsedHeight,
+                  flexibleSpace: FlexibleSpaceBar(
+                    centerTitle: false,
+                    collapseMode: CollapseMode.parallax,
+                    expandedTitleScale: expandedTitleScale,
+                    titlePadding: EdgeInsets.zero,
+                    title: cart.products.products.isEmpty
+                        ? const SizedBox()
+                        : Padding(
+                            padding: EdgeInsets.all(uiConstants.paddingSmall),
+                            child: Align(
+                              alignment: Alignment.bottomRight,
+                              child: ElevatedButton.icon(
+                                style: ElevatedButton.styleFrom(
+                                    visualDensity: VisualDensity.compact),
+                                onPressed: () {
+                                  Provider.of<ProductCart>(context,
+                                          listen: false)
+                                      .clearProducts();
 
-                                Provider.of<ProductList>(context, listen: false)
-                                    .clearProductSelectedQuantity();
-                              },
-                              label: Text(
-                                'Limpar carrinho',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .labelSmall
-                                    ?.copyWith(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .tertiary,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                              ),
-                              icon: Icon(
-                                Icons.remove_shopping_cart_rounded,
-                                size: uiConstants.iconSizeSmall,
-                                color: Theme.of(context).colorScheme.tertiary,
+                                  Provider.of<ProductList>(context,
+                                          listen: false)
+                                      .clearProductSelectedQuantity();
+                                },
+                                label: Text(
+                                  'Limpar carrinho',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelSmall
+                                      ?.copyWith(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .tertiary,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                ),
+                                icon: Icon(
+                                  Icons.remove_shopping_cart_rounded,
+                                  size: uiConstants.iconSizeSmall,
+                                  color: Theme.of(context).colorScheme.tertiary,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                )),
-            SliverList.list(
-              children: [
-                cart.products.products.isEmpty
-                    ? SizedBox(
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.shopping_basket_rounded,
-                                size: uiConstants.iconSizeExtraLarge,
-                                color: uiConstants.jeanGrey,
-                              ),
-                              const Text('Seu carrinho está vazio'),
-                            ],
-                          ),
-                        ),
-                      )
-                    : Column(
-                        children: [
-                          ListView.builder(
-                            // never scrollable
-                            physics: const NeverScrollableScrollPhysics(),
-                            shrinkWrap: true,
-                            itemCount: cart.products.products.length,
-                            itemBuilder: (context, index) {
-                              final product = cart.products.products[index];
-                              return ListTile(
-                                isThreeLine: true,
-                                title: Text(product.productName),
-                                subtitle: Row(
-                                  children: [
-                                    Text(
-                                        '${product.productUnitQuantity} ${product.productUnitOfMeasurement} x R\$ ${product.productPrice.toStringAsFixed(2)} = '),
-                                    Text(
-                                        'R\$ ${product.productPrice.toStringAsFixed(2)}'),
-                                  ],
+                  )),
+              SliverList.list(
+                children: [
+                  cart.products.products.isEmpty
+                      ? SizedBox(
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.shopping_basket_rounded,
+                                  size: uiConstants.iconSizeExtraLarge,
+                                  color: uiConstants.jeanGrey,
                                 ),
-                                trailing: IconButton(
-                                  icon: const Icon(Icons.delete),
-                                  onPressed: () {
-                                    Provider.of<ProductCart>(context,
-                                            listen: false)
-                                        .removeProduct(product);
-                                    product.decrementSelectedQuantity(context);
-                                    Provider.of<ProductList>(context,
-                                            listen: false)
-                                        .updateProduct(product);
-                                  },
-                                ),
-                              );
-                            },
+                                const Text('Seu carrinho está vazio'),
+                              ],
+                            ),
                           ),
-                          const Divider(),
-                          ListTile(
-                            title: const Text('Total'),
-                            subtitle: Text(
-                                'R\$ ${cart.totalPrice.toStringAsFixed(2)}'),
-                          ),
-                        ],
-                      ),
-              ],
+                        )
+                      : Column(
+                          children: [
+                            ListView.builder(
+                              // never scrollable
+                              physics: const NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
+                              itemCount: cart.products.products.length,
+                              itemBuilder: (context, index) {
+                                final product = cart.products.products[index];
+                                return ListTile(
+                                  isThreeLine: true,
+                                  title: Text(product.productName),
+                                  subtitle: Row(
+                                    children: [
+                                      Text(
+                                          '${product.productUnitQuantity} ${product.productUnitOfMeasurement} x R\$ ${product.productPrice.toStringAsFixed(2)} = '),
+                                      Text(
+                                          'R\$ ${product.productPrice.toStringAsFixed(2)}'),
+                                    ],
+                                  ),
+                                  trailing: IconButton(
+                                    icon: const Icon(Icons.delete),
+                                    onPressed: () {
+                                      Provider.of<ProductCart>(context,
+                                              listen: false)
+                                          .removeProduct(product);
+                                      product
+                                          .decrementSelectedQuantity(context);
+                                      Provider.of<ProductList>(context,
+                                              listen: false)
+                                          .updateProduct(product);
+                                    },
+                                  ),
+                                );
+                              },
+                            ),
+                            const Divider(),
+                          ],
+                        ),
+                ],
+              ),
+            ],
+          ),
+          bottomNavigationBar: ListTile(
+            title: const Text('Total'),
+            subtitle: Text('R\$ ${cart.totalPrice.toStringAsFixed(2)}'),
+            trailing: ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                visualDensity: VisualDensity.standard,
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+                padding: EdgeInsets.symmetric(
+                    horizontal: uiConstants.paddingMedium,
+                    vertical: uiConstants.paddingSmall),
+              ),
+              onPressed: () {},
+              icon: Icon(
+                Icons.payment_rounded,
+                size: uiConstants.iconSizeSmall,
+                color: Theme.of(context).colorScheme.onSecondary,
+              ),
+              label: Text(
+                'Pagar',
+                style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                      color: Theme.of(context).colorScheme.onSecondary,
+                    ),
+              ),
             ),
-          ],
+          ),
         );
       },
     );
