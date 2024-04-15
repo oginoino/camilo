9340f95e516bd3c79bd4b4ffc414bd3d634cb0d3
Gino <gino.laranjeiras@onikel.com>
2024-04-15 01:55:39 -0300

    Adicionar componente SliverToBoxAdapter para exibir informações de entrega na classe HomePage

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -17,10 +17,63 @@ class HomePage extends StatelessWidget {
       return CustomScrollView(
         slivers: [
           const CustomAppBar(),
+          _buildSliverTopBoxAdapter(context),
           _buildPageBody(context, productCategories),
         ],
       );
     });
+  }
+
+  SliverToBoxAdapter _buildSliverTopBoxAdapter(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+          height: 60,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.background,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.shopping_bag_rounded,
+                      size: uiConstants.iconSizeSmall,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                    SizedBox(width: uiConstants.paddingExtraSmall),
+                    Text(
+                      'Mercado aberto agora!',
+                      style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Icon(
+                    //   Icons.shopping_bag_rounded,
+                    //   size: uiConstants.iconSizeSmall,
+                    //   color: Theme.of(context).colorScheme.tertiary,
+                    // ),
+                    // SizedBox(width: uiConstants.paddingExtraSmall),
+                    Text(
+                      'Entrega em até 30 minutos',
+                      style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                            color: Theme.of(context).colorScheme.tertiary,
+                          ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )),
+    );
   }
 
   SliverList _buildPageBody(
@@ -29,7 +82,6 @@ class HomePage extends StatelessWidget {
       Padding(
         padding: EdgeInsets.symmetric(
           horizontal: uiConstants.paddingSmall,
-          vertical: uiConstants.paddingMedium,
         ),
         child: ListView.builder(
           shrinkWrap: true,
