bbebd6654501ac2c646eb4a54c9d90be765c89a7
Gino <gino.laranjeiras@onikel.com>
2024-04-30 15:14:21 -0300

    Adicionar página de recuperação de senha

---
+++ lib/ui/pages/forgot_password_page.dart
@@ -0,0 +1,33 @@
+import '../../common_libs.dart';
+
+class ForgotPasswordPage extends StatelessWidget {
+  const ForgotPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        surfaceTintColor: uiConstants.backgroundLight,
+        leading: IconButton(
+          icon: const Icon(Icons.keyboard_arrow_left_rounded),
+          onPressed: () {
+            appRouter.canPop()
+                ? appRouter.pop()
+                : appRouter.go(ScreenPaths.login);
+          },
+        ),
+      ),
+      body: const SingleChildScrollView(
+        reverse: true,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              AppLogo(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
