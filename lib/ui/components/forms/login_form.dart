ac201bc7a69b72c8007cc9449c8f351ca99831e2
Gino <gino.laranjeiras@onikel.com>
2024-04-30 13:18:11 -0300

    Atualizar definições de cores no tema claro e componente CustomProductModalBottomSheet

--- lib/ui/components/forms/login_form.dart
+++ lib/ui/components/forms/login_form.dart
@@ -28,100 +28,105 @@ class LoginForm extends StatelessWidget {
   Widget build(BuildContext context) {
     const String hintTextEmail = 'Email';
     const String hintTextPassword = 'Senha';
+    FocusNode emailFocusNode = FocusNode();
+    FocusNode passwordFocusNode = FocusNode();
     return Padding(
       padding: EdgeInsets.all(uiConstants.paddingExtraLarge),
-      child: Form(
-        key: _loginFormKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              title,
-              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                    color: Theme.of(context).colorScheme.tertiary,
-                    fontWeight: FontWeight.w600,
-                  ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: uiConstants.paddingExtraExtraLarge,
-            ),
-            TextFormField(
-              key: Key(_emailLoginInputKey),
-              controller: _emailLoginController,
-              keyboardType: TextInputType.emailAddress,
-              autofillHints: const [AutofillHints.email],
-              decoration: const InputDecoration(
-                hintText: hintTextEmail,
-              ),
-              validator: (String? value) {
-                return null;
-              },
-              onEditingComplete: () {
-                FocusScope.of(context).nextFocus();
-              },
-              // remove focus on click outside
-              onTapOutside: ((event) => FocusScope.of(context).unfocus()),
-            ),
-            SizedBox(
-              height: uiConstants.paddingExtraLarge,
-            ),
-            TextFormField(
-              key: Key(_emailPasswordInputKey),
-              controller: _passwordLoginController,
-              obscureText: true,
-              keyboardType: TextInputType.visiblePassword,
-              autofillHints: const [AutofillHints.password],
-              decoration: const InputDecoration(
-                hintText: hintTextPassword,
-              ),
-              validator: (String? value) {
-                return null;
-              },
-              onEditingComplete: () {
-                FocusScope.of(context).unfocus();
-              },
-              // remove focus on click outside
-              onTapOutside: ((event) => FocusScope.of(context).unfocus()),
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                onPressed: () {},
-                child: Text(
-                  forgotPasswordButtonText,
-                  style: Theme.of(context).textTheme.labelLarge?.copyWith(
+      child: FocusScope(
+        child: Form(
+          key: _loginFormKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                title,
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                       color: Theme.of(context).colorScheme.tertiary,
-                      fontWeight: FontWeight.w600),
+                      fontWeight: FontWeight.w600,
+                    ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: uiConstants.paddingExtraExtraLarge,
+              ),
+              TextFormField(
+                key: Key(_emailLoginInputKey),
+                controller: _emailLoginController,
+                keyboardType: TextInputType.emailAddress,
+                autofillHints: const [AutofillHints.email],
+                decoration: const InputDecoration(
+                  hintText: hintTextEmail,
+                ),
+                validator: (String? value) {
+                  return null;
+                },
+                onEditingComplete: () {
+                  emailFocusNode.unfocus();
+                  passwordFocusNode.requestFocus();
+                },
+                onTapOutside: ((event) => emailFocusNode.unfocus()),
+                focusNode: emailFocusNode,
+              ),
+              SizedBox(
+                height: uiConstants.paddingExtraLarge,
+              ),
+              TextFormField(
+                key: Key(_emailPasswordInputKey),
+                controller: _passwordLoginController,
+                obscureText: true,
+                keyboardType: TextInputType.visiblePassword,
+                autofillHints: const [AutofillHints.password],
+                decoration: const InputDecoration(
+                  hintText: hintTextPassword,
+                ),
+                validator: (String? value) {
+                  return null;
+                },
+                onEditingComplete: () {
+                  passwordFocusNode.unfocus();
+                },
+                onTapOutside: ((event) => passwordFocusNode.unfocus()),
+                focusNode: passwordFocusNode,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    forgotPasswordButtonText,
+                    style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                        color: Theme.of(context).colorScheme.tertiary,
+                        fontWeight: FontWeight.w600),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: uiConstants.paddingExtraLarge,
-            ),
-            FilledButton(
-              onPressed: () {},
-              child: Text(ctaButtonText),
-            ),
-            SizedBox(
-              height: uiConstants.paddingExtraLarge,
-            ),
-            Text(
-              helperRegisterButtonText,
-              style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
-              textAlign: TextAlign.center,
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text(registerButtonText,
-                  style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                        color: Theme.of(context).colorScheme.tertiary,
-                        fontWeight: FontWeight.w600,
-                      )),
-            ),
-          ],
+              SizedBox(
+                height: uiConstants.paddingExtraLarge,
+              ),
+              FilledButton(
+                onPressed: () {},
+                child: Text(ctaButtonText),
+              ),
+              SizedBox(
+                height: uiConstants.paddingExtraLarge,
+              ),
+              Text(
+                helperRegisterButtonText,
+                style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+                textAlign: TextAlign.center,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(registerButtonText,
+                    style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                          color: Theme.of(context).colorScheme.tertiary,
+                          fontWeight: FontWeight.w600,
+                        )),
+              ),
+            ],
+          ),
         ),
       ),
     );
