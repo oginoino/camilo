925769dd99c0f21a339a6c0ab7195f9701030428
Gino <gino.laranjeiras@onikel.com>
2024-05-09 19:10:51 -0300

    refactor: Update CustomPageBody to handle empty product list
    The CustomPageBody component in custom_page_body.dart has been updated to handle the case when the product list is empty. It now displays a message and a button to fetch all products. This improves the user experience by providing a clear indication when no products are found and allowing the user to easily retrieve the products.

--- lib/ui/components/custom_page_body.dart
+++ lib/ui/components/custom_page_body.dart
@@ -11,20 +11,42 @@ class CustomPageBody extends StatelessWidget {
       children: [
         Consumer<ProductList>(builder: (context, products, child) {
           List<String> productCategories = products.categories;
-          return Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: uiConstants.paddingSmall,
-            ),
-            child: ListView.builder(
+          if (products.products.isNotEmpty) {
+            return ListView.builder(
               shrinkWrap: true,
               physics: const NeverScrollableScrollPhysics(),
               itemCount: productCategories.length,
               itemBuilder: (context, index) {
                 return ProductCarossel(
-                    category: productCategories[index], products: products);
+                  category: productCategories[index],
+                  products: products,
+                );
               },
-            ),
-          );
+            );
+          } else {
+            return SizedBox(
+              height: MediaQuery.sizeOf(context).height * 0.5,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.search_off_rounded,
+                        size: 100, color: uiConstants.primaryLight),
+                    const Text(
+                      'Nenhum produto foi encontrado para o termo buscado.',
+                      textAlign: TextAlign.center,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        products.getAllProducts();
+                      },
+                      child: const Text('Ver tudo'),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
         })
       ],
     );
