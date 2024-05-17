b742aec49f798442697331c1828a8833b67a8482
Gino <gino.laranjeiras@onikel.com>
2024-05-17 19:58:30 -0300

    feat: Adicionar FloatingActionButton para ir para o carrinho na página inicial

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -5,20 +5,45 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          CustomAppBar(),
-          CustomPageBody(),
-        ],
-      ),
-      floatingActionButton: SizedBox(),
-      extendBody: true,
-      resizeToAvoidBottomInset: false,
-      primary: true,
-      key: Key('home_page'),
-      restorationId: 'home_page',
-      drawer: CustomDrawer(),
+    return Consumer<ProductCart>(builder: (
+      context,
+      productCart,
+      child,
+    ) {
+      return Scaffold(
+        body: const CustomScrollView(
+          slivers: [
+            CustomAppBar(),
+            CustomPageBody(),
+          ],
+        ),
+        floatingActionButton:
+            productCart.isMinimumOrder ? const FABCart() : null,
+        extendBody: true,
+        resizeToAvoidBottomInset: false,
+        primary: true,
+        key: const Key('home_page'),
+        restorationId: 'home_page',
+        drawer: const CustomDrawer(),
+      );
+    });
+  }
+}
+
+class FABCart extends StatelessWidget {
+  const FABCart({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.extended(
+      onPressed: () {
+        appRouter.push(ScreenPaths.cart);
+      },
+      label: const Text('Ir para o carrinho'),
+      icon: const Icon(Icons.shopping_cart),
+      key: const Key('fab_cart'),
     );
   }
 }
