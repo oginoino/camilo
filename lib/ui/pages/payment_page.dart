f76e14ace1c403ed7f568761489792a2b451f5a3
Gino <gino.laranjeiras@onikel.com>
2024-05-17 16:26:46 -0300

    Atualizar página de pagamento para definir o status do pagamento como 'expirado' e voltar à página anterior

--- lib/ui/pages/payment_page.dart
+++ lib/ui/pages/payment_page.dart
@@ -172,6 +172,7 @@ class TimerWidgetState extends State<TimerWidget> {
   void _expirePayment() {
     setState(() {
       widget.checkout.payment?.setStatus('expired');
+      appRouter.pop();
     });
   }
 
